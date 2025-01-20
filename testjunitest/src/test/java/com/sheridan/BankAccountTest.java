a778fc92e7a11a5f40cee4d280f82868f2a6cb20
Manh Phu Giang <gmanhphuu@gmail.com>
2025-01-19 23:48:11 -0500

    test display name added

--- testjunitest/src/test/java/com/sheridan/BankAccountTest.java
+++ testjunitest/src/test/java/com/sheridan/BankAccountTest.java
@@ -5,15 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
 import org.junit.jupiter.api.*;
-
+@DisplayName("BankAccountTest")
 public class BankAccountTest {
     @Test
+    @DisplayName("withderaw test $400, initial $500, min -$1000")
     public void testWithdraw() {
         BankAccount bankAccount = new BankAccount(500, -1000);
         bankAccount.withdraw(400);
         assertEquals(100, bankAccount.getBalance(), 0.001);
     }
     @Test
+    @DisplayName("deposit test $200, initial $500, min -$1000")
     public void testDeposit() {
         BankAccount bankAccount = new BankAccount(500, -1000);
         bankAccount.deposit(200);
